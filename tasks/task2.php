9b22dbfd4b8a29728573d4c2fc44a9c43d79017b
Amany <amanyhamza196@gmail.com>
2022-03-07 18:58:28 +0200

    t2

--- tasks/task2.php
+++ tasks/task2.php
@@ -1,9 +1,13 @@
 <?php
 
-$str='v';
-$nextStr=++$str;
-if(strlen($nextStr)>1){
-    $nextStr=$nextStr[0];
+function nextChar($str){
+    $nextStr=++$str;
+    if(strlen($nextStr)>1){
+        $nextStr=$nextStr[0];
+    }
+    echo $nextStr;
 }
-echo $nextStr;
+
+nextChar('r')
+
 ?>
